1643bc961be2201d29cec8fad9d1507a76311b82
hihihihi30 <dhtmdgn77@gmail.com>
2025-09-10 10:23:22 +0900

    202509101022_Init
    깃허브 연동 테스트

---
+++ Assets/GithubTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //github 테스트 중 
+        // 너무 졸리다 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
